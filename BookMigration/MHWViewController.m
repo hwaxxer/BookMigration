8a75c69200efc32a1721c2cdd76e1aee9331f646
Martin Hwasser <martin.hwasser@gmail.com>
2013-08-26 22:21:09 +0200

    Initial Commit

---
+++ BookMigration/MHWViewController.m
@@ -0,0 +1,29 @@
+//
+//  MHWViewController.m
+//  BookMigration
+//
+//  Created by Martin Hwasser on 8/26/13.
+//  Copyright (c) 2013 Martin Hwasser. All rights reserved.
+//
+
+#import "MHWViewController.h"
+
+@interface MHWViewController ()
+
+@end
+
+@implementation MHWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
